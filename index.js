c1f3fcdd3e2974d3e351b64ab40a8675a2358e1a
dselca <dselca@gmail.com>
2018-10-25 14:27:52 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var thing = {
+    itemName: "",
+    itemPrice: Number
+  };
+  return thing;
 }
 
 function viewCart() {
